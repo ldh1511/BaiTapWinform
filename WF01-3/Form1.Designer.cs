992c287e06f18d4b5b4aee4fc75cbce54ad67d7e
ldh1511 <53638313+ldh1511@users.noreply.github.com>
2020-07-02 20:41:39 +0700

    Add new projet

---
+++ WF01-3/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace WF01_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_Ten = new System.Windows.Forms.Label();
+            this.tb_Nhap = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_Dv1 = new System.Windows.Forms.CheckBox();
+            this.cb_Dv4 = new System.Windows.Forms.CheckBox();
+            this.cb_Dv3 = new System.Windows.Forms.CheckBox();
+            this.cb_Dv2 = new System.Windows.Forms.CheckBox();
+            this.lb_Gt1 = new System.Windows.Forms.Label();
+            this.lb_Gt2 = new System.Windows.Forms.Label();
+            this.lb_Gt3 = new System.Windows.Forms.Label();
+            this.lb_Gt4 = new System.Windows.Forms.Label();
+            this.lb_Gia = new System.Windows.Forms.Label();
+            this.lb_Dv5 = new System.Windows.Forms.Label();
+            this.lb_Gt5 = new System.Windows.Forms.Label();
+            this.lb_TongTien = new System.Windows.Forms.Label();
+            this.tb_HoaDon = new System.Windows.Forms.TextBox();
+            this.bt_Tinh = new System.Windows.Forms.Button();
+            this.bt_Close = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_Ten
+            // 
+            this.lb_Ten.AutoSize = true;
+            this.lb_Ten.Location = new System.Drawing.Point(13, 35);
+            this.lb_Ten.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_Ten.Name = "lb_Ten";
+            this.lb_Ten.Size = new System.Drawing.Size(107, 16);
+            this.lb_Ten.TabIndex = 0;
+            this.lb_Ten.Text = "Tên khách hàng:";
+            // 
+            // tb_Nhap
+            // 
+            this.tb_Nhap.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_Nhap.Location = new System.Drawing.Point(127, 32);
+            this.tb_Nhap.Name = "tb_Nhap";
+            this.tb_Nhap.Size = new System.Drawing.Size(247, 22);
+            this.tb_Nhap.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.Khaki;
+            this.label1.Location = new System.Drawing.Point(12, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 187);
+            this.label1.TabIndex = 11;
+            // 
+            // cb_Dv1
+            // 
+            this.cb_Dv1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cb_Dv1.AutoSize = true;
+            this.cb_Dv1.BackColor = System.Drawing.Color.Khaki;
+            this.cb_Dv1.Location = new System.Drawing.Point(26, 117);
+            this.cb_Dv1.Name = "cb_Dv1";
+            this.cb_Dv1.Size = new System.Drawing.Size(73, 20);
+            this.cb_Dv1.TabIndex = 13;
+            this.cb_Dv1.Text = "Cạo vôi";
+            this.cb_Dv1.UseVisualStyleBackColor = false;
+            // 
+            // cb_Dv4
+            // 
+            this.cb_Dv4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cb_Dv4.AutoSize = true;
+            this.cb_Dv4.BackColor = System.Drawing.Color.Khaki;
+            this.cb_Dv4.Location = new System.Drawing.Point(26, 195);
+            this.cb_Dv4.Name = "cb_Dv4";
+            this.cb_Dv4.Size = new System.Drawing.Size(84, 20);
+            this.cb_Dv4.TabIndex = 14;
+            this.cb_Dv4.Text = "Tẩy trắng";
+            this.cb_Dv4.UseVisualStyleBackColor = false;
+            // 
+            // cb_Dv3
+            // 
+            this.cb_Dv3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cb_Dv3.AutoSize = true;
+            this.cb_Dv3.BackColor = System.Drawing.Color.Khaki;
+            this.cb_Dv3.Location = new System.Drawing.Point(26, 169);
+            this.cb_Dv3.Name = "cb_Dv3";
+            this.cb_Dv3.Size = new System.Drawing.Size(85, 20);
+            this.cb_Dv3.TabIndex = 15;
+            this.cb_Dv3.Text = "Chụp hình";
+            this.cb_Dv3.UseVisualStyleBackColor = false;
+            // 
+            // cb_Dv2
+            // 
+            this.cb_Dv2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cb_Dv2.AutoSize = true;
+            this.cb_Dv2.BackColor = System.Drawing.Color.Khaki;
+            this.cb_Dv2.Location = new System.Drawing.Point(26, 143);
+            this.cb_Dv2.Name = "cb_Dv2";
+            this.cb_Dv2.Size = new System.Drawing.Size(75, 20);
+            this.cb_Dv2.TabIndex = 16;
+            this.cb_Dv2.Text = "Lấy cao";
+            this.cb_Dv2.UseVisualStyleBackColor = false;
+            // 
+            // lb_Gt1
+            // 
+            this.lb_Gt1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_Gt1.AutoSize = true;
+            this.lb_Gt1.BackColor = System.Drawing.Color.Khaki;
+            this.lb_Gt1.Location = new System.Drawing.Point(276, 118);
+            this.lb_Gt1.Name = "lb_Gt1";
+            this.lb_Gt1.Size = new System.Drawing.Size(84, 16);
+            this.lb_Gt1.TabIndex = 17;
+            this.lb_Gt1.Text = "100 000 VNĐ";
+            // 
+            // lb_Gt2
+            // 
+            this.lb_Gt2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_Gt2.AutoSize = true;
+            this.lb_Gt2.BackColor = System.Drawing.Color.Khaki;
+            this.lb_Gt2.Location = new System.Drawing.Point(266, 144);
+            this.lb_Gt2.Name = "lb_Gt2";
+            this.lb_Gt2.Size = new System.Drawing.Size(94, 16);
+            this.lb_Gt2.TabIndex = 18;
+            this.lb_Gt2.Text = "1 200 000 VNĐ";
+            // 
+            // lb_Gt3
+            // 
+            this.lb_Gt3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_Gt3.AutoSize = true;
+            this.lb_Gt3.BackColor = System.Drawing.Color.Khaki;
+            this.lb_Gt3.Location = new System.Drawing.Point(276, 170);
+            this.lb_Gt3.Name = "lb_Gt3";
+            this.lb_Gt3.Size = new System.Drawing.Size(84, 16);
+            this.lb_Gt3.TabIndex = 19;
+            this.lb_Gt3.Text = "150 000 VNĐ";
+            // 
+            // lb_Gt4
+            // 
+            this.lb_Gt4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_Gt4.AutoSize = true;
+            this.lb_Gt4.BackColor = System.Drawing.Color.Khaki;
+            this.lb_Gt4.Location = new System.Drawing.Point(276, 196);
+            this.lb_Gt4.Name = "lb_Gt4";
+            this.lb_Gt4.Size = new System.Drawing.Size(84, 16);
+            this.lb_Gt4.TabIndex = 20;
+            this.lb_Gt4.Text = "100 000 VNĐ";
+            // 
+            // lb_Gia
+            // 
+            this.lb_Gia.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_Gia.AutoSize = true;
+            this.lb_Gia.BackColor = System.Drawing.Color.Khaki;
+            this.lb_Gia.Location = new System.Drawing.Point(23, 81);
+            this.lb_Gia.Name = "lb_Gia";
+            this.lb_Gia.Size = new System.Drawing.Size(62, 16);
+            this.lb_Gia.TabIndex = 12;
+            this.lb_Gia.Text = "Bảng giá";
+            // 
+            // lb_Dv5
+            // 
+            this.lb_Dv5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_Dv5.AutoSize = true;
+            this.lb_Dv5.BackColor = System.Drawing.Color.Khaki;
+            this.lb_Dv5.Location = new System.Drawing.Point(23, 222);
+            this.lb_Dv5.Name = "lb_Dv5";
+            this.lb_Dv5.Size = new System.Drawing.Size(63, 16);
+            this.lb_Dv5.TabIndex = 21;
+            this.lb_Dv5.Text = "Hàn răng";
+            // 
+            // lb_Gt5
+            // 
+            this.lb_Gt5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_Gt5.AutoSize = true;
+            this.lb_Gt5.BackColor = System.Drawing.Color.Khaki;
+            this.lb_Gt5.Location = new System.Drawing.Point(258, 222);
+            this.lb_Gt5.Name = "lb_Gt5";
+            this.lb_Gt5.Size = new System.Drawing.Size(102, 16);
+            this.lb_Gt5.TabIndex = 23;
+            this.lb_Gt5.Text = "90 000 VNĐ/ cái";
+            // 
+            // lb_TongTien
+            // 
+            this.lb_TongTien.AutoSize = true;
+            this.lb_TongTien.Location = new System.Drawing.Point(12, 284);
+            this.lb_TongTien.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_TongTien.Name = "lb_TongTien";
+            this.lb_TongTien.Size = new System.Drawing.Size(64, 16);
+            this.lb_TongTien.TabIndex = 24;
+            this.lb_TongTien.Text = "Tổng tiền";
+            // 
+            // tb_HoaDon
+            // 
+            this.tb_HoaDon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_HoaDon.Location = new System.Drawing.Point(154, 281);
+            this.tb_HoaDon.Name = "tb_HoaDon";
+            this.tb_HoaDon.ReadOnly = true;
+            this.tb_HoaDon.Size = new System.Drawing.Size(180, 22);
+            this.tb_HoaDon.TabIndex = 25;
+            this.tb_HoaDon.Text = "0";
+            this.tb_HoaDon.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // bt_Tinh
+            // 
+            this.bt_Tinh.Location = new System.Drawing.Point(16, 329);
+            this.bt_Tinh.Name = "bt_Tinh";
+            this.bt_Tinh.Size = new System.Drawing.Size(118, 26);
+            this.bt_Tinh.TabIndex = 26;
+            this.bt_Tinh.Text = "Tính tiền";
+            this.bt_Tinh.UseVisualStyleBackColor = true;
+            this.bt_Tinh.Click += new System.EventHandler(this.bt_Tinh_Click);
+            // 
+            // bt_Close
+            // 
+            this.bt_Close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bt_Close.Location = new System.Drawing.Point(243, 329);
+            this.bt_Close.Name = "bt_Close";
+            this.bt_Close.Size = new System.Drawing.Size(118, 26);
+            this.bt_Close.TabIndex = 27;
+            this.bt_Close.Text = "Thoát";
+            this.bt_Close.UseVisualStyleBackColor = true;
+            this.bt_Close.Click += new System.EventHandler(this.bt_Close_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(92, 220);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(42, 22);
+            this.numericUpDown1.TabIndex = 28;
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(386, 375);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.bt_Close);
+            this.Controls.Add(this.bt_Tinh);
+            this.Controls.Add(this.tb_HoaDon);
+            this.Controls.Add(this.lb_TongTien);
+            this.Controls.Add(this.lb_Gt5);
+            this.Controls.Add(this.lb_Dv5);
+            this.Controls.Add(this.lb_Gt4);
+            this.Controls.Add(this.lb_Gt3);
+            this.Controls.Add(this.lb_Gt2);
+            this.Controls.Add(this.lb_Gt1);
+            this.Controls.Add(this.cb_Dv2);
+            this.Controls.Add(this.cb_Dv3);
+            this.Controls.Add(this.cb_Dv4);
+            this.Controls.Add(this.cb_Dv1);
+            this.Controls.Add(this.lb_Gia);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_Nhap);
+            this.Controls.Add(this.lb_Ten);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tính tiền khám răng";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_Ten;
+        private System.Windows.Forms.TextBox tb_Nhap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cb_Dv1;
+        private System.Windows.Forms.CheckBox cb_Dv4;
+        private System.Windows.Forms.CheckBox cb_Dv3;
+        private System.Windows.Forms.CheckBox cb_Dv2;
+        private System.Windows.Forms.Label lb_Gt1;
+        private System.Windows.Forms.Label lb_Gt2;
+        private System.Windows.Forms.Label lb_Gt3;
+        private System.Windows.Forms.Label lb_Gt4;
+        private System.Windows.Forms.Label lb_Gia;
+        private System.Windows.Forms.Label lb_Dv5;
+        private System.Windows.Forms.Label lb_Gt5;
+        private System.Windows.Forms.Label lb_TongTien;
+        private System.Windows.Forms.TextBox tb_HoaDon;
+        private System.Windows.Forms.Button bt_Tinh;
+        private System.Windows.Forms.Button bt_Close;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ WF01-3/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF01_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int s1=100000, s2=1200000, s3=150000, s4=100000, s5=90000;
+        
+        private void bt_Tinh_Click(object sender, EventArgs e)
+        {
+
+            TongTien();
+;        }
+        private void bt_Close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        public void TongTien()
+        {
+            int sl, sum = 0;
+            if (cb_Dv1.CheckState == CheckState.Checked)
+            {
+                sum += s1;
+            }
+            if (cb_Dv2.CheckState == CheckState.Checked)
+            {
+                sum += s2;
+            }
+            if (cb_Dv3.CheckState == CheckState.Checked)
+            {
+                sum += s3;
+            }
+            if (cb_Dv4.CheckState == CheckState.Checked)
+            {
+                sum += s4;
+            }
+            sl = Convert.ToInt32(numericUpDown1.Value);
+            sum += sl * s5;
+            tb_HoaDon.Text = Convert.ToString(sum);
+        }
+    }
+}
